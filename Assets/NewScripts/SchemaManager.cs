f4e71954985ddcd7aff512bd8fd7810f8ec61929
nh1013 <nicholas.huang13@imperial.ac.uk>
2017-08-19 17:03:05 +0100

    Outline for SchemaManager

--- Assets/NewScripts/SchemaManager.cs
+++ Assets/NewScripts/SchemaManager.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class SchemaManager : MonoBehaviour {
 
+    public List<Transform> m_tableList;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+    // Load a schema and create tables
+    void LoadSchema() {
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
